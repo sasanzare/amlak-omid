d6ac8f34443cd54897b1c29089ce36dff63d71a6
sasanzare <sasanzare75@gmail.com>
2022-09-20 18:42:42 +0430

    Edite BgTp components

--- components/bgTop/BgTop.js
+++ components/bgTop/BgTop.js
@@ -1,8 +1,9 @@
-function BgTop({img,content=null}) { 
+function BgTop(props) { 
   return (
-    <div className="BgTop bg-cover text-center d-flex align-items-center justify-content-center" style={{ backgroundImage: `url(${img})` 
+    <div className="BgTop bg-cover text-center d-flex flex-column align-items-center justify-content-center" style={{ backgroundImage: `url(${props.img})` 
       }}>
-         <h1 className="text-white h2 fw-bold text-shadow">{content}</h1>  
+         <h1 className={`text-white h2 fw-bold text-shadow ${props.myClass}`}>{props.content}</h1>
+         {props.children}  
     </div>
   );
 }
