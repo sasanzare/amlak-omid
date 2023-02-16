b65a0f55fa17cc00be50d01f7e90ff2b4c0194e4
sasanzare <sasanzare75@gmail.com>
2023-02-16 04:42:19 +0330

    create search page

--- pages/Search/index.tsx
+++ pages/Search/index.tsx
@@ -3,13 +3,22 @@ import { Container, Row, Col } from 'react-bootstrap';
 import BuySearch from '../../blocks/search/BuySearch';
 import SugessBox from '../../blocks/sugess/SugessBox';
 import PaginationPage from '../../components/PaginationPage';
-import { toast } from 'react-toastify';
-// import axios from 'axios';
-import { PropertiesApi, STATICS,searchRealEstateApi } from "../../api";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
+import Estate from "../../components/Estate";
+import { PropertiesApi, STATICS,searchRealEstateApi,getRealEstateApi } from "../../api";
 import { context } from '../../context/index';
 // import ReactPaginate from 'react-paginate';
 import { useRouter } from 'next/router'
-
+import {
+  property,
+  room,
+  meterage,
+  assignment,
+  advertisingStatus,
+} from "../../lib/enum-converter";
+import moment from 'jalali-moment';
+import Link from "next/link";
 export async function getServerSideProps(context: any) {
   return {
     props: {}, // will be passed to the page component as props
@@ -22,21 +31,31 @@ const Buy = (props: any) => {
   const [pageCount, setPageCount] = useState(10);
   const { setShowLoading } = useContext(context);
   const [properties, setProperties] = useState([]);
+  const [realEstateList, setRealEstateList] = useState([]);
 
 
   useEffect(() => {
-    fetchData({ selected: router.query.pageNumber })
+    // fetchData({ selected: router.query.pageNumber })
+    getRealEstate()
   }, []);
 
 
   function getRealEstate() {
+    // let searchQuery: string[] = [];
+    // router.query.pageNumber = event.selected || 1;
+    // console.log(router.query)
+    // Object.keys(router.query).forEach((e) => {
+    //   searchQuery.push(`${e}=${router.query[e]}`);
+    // })
+
     setShowLoading(true);
     axios
-      .get(getRealEstateApi)
+      .post(searchRealEstateApi, router.query)
       .then((res) => {
         setRealEstateList(res.data);
         if (res.status === 200) {
           setShowLoading(false);
+        
         }
       })
       .catch((err) => {
@@ -61,7 +80,7 @@ const Buy = (props: any) => {
       searchQuery.push(`${e}=${router.query[e]}`);
       // console.log(`${e}=${router.query[e]}`)
     })
-    console.log(searchQuery)
+    console.log(typeof searchQuery)
     setShowLoading(true);
     // console.log(PropertiesApi + `/search?${searchQuery.join('&')}`);
     fetch(PropertiesApi + `/search?${searchQuery.join('&')}`).then((res) => {
@@ -82,15 +101,54 @@ const Buy = (props: any) => {
   };
 
 
+  const getIdRealEstate = (e) => {
+    router.push(`/Rent/${e.target.getAttribute("data-reactid")}`)
+};
 
   return (
     <Container className="Home pt-5 mt-5 pb-4">
       
-      <Row>
+      {/* <Row>
         <Col lg={12} sm={12} className='mx-auto'><BuySearch onChange={(e) => { }} /></Col>
-      </Row>
+      </Row> */}
 
-      <SugessBox properties={properties} />
+      {/* <SugessBox properties={properties} /> */}
+<Row>
+
+
+{realEstateList.length > 0 ? (
+      realEstateList.map((data) => {
+        return (<Estate
+          key={data.id}
+          myClass="p-sm-2 p-3 my-lg-0  my-2 col-lg-3 col-md-6 col-11 mx-auto"
+          img={data.estateImage}
+          title={(data.agency)? data.agency.name : "کاربر عادی" }
+          profile={(data.agency)? "/uploads/advertising/"+data.agency.agencyImage : "/img/avatar.jpeg" }
+          location={data?.cityArea?.name}
+          price={data.price.replace(/(\d)(?=(\d{3})+$)/g, '$1,')}
+          bed={room(data.roomCount)}
+          type={property(data.type)}
+          time={moment(data.createdAt, 'YYYY/MM/DD').locale('fa').format('YYYY/MM/DD')}
+          meter={meterage(data.meter)}
+          phoneNumber={data.phoneNumber}
+          to={data.id}
+          getId={getIdRealEstate}
+        />);
+        })
+      ) : (
+        <div className='col-12 d-flex justify-content-center align-items-center vh-80'>
+            <div className='text-center'>
+            <h1 className='h4 text-es text-center'>متاسفانه آگهی با فیلتر‌های مدنظر شما پیدا نشد!</h1>
+          <Link href="/">
+                  <a className="btn btn-es col-md-6 col-sm-7 col-9 rounded-3 mt-5 he-fit">
+                    بازگشت به خانه
+                  </a>
+                </Link>
+            </div>
+        </div>
+      )}
+
+</Row>
 
       {/* <ReactPaginate
         breakLabel="..."
